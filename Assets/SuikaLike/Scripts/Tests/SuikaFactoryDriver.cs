d25cb6fc1682edd42a664d8abbc6dfa3bd72c518
nakano yosuke <y.nakano@vrhikky.com>
2024-05-25 17:44:04 +0900

    ファクトリのドライバー

---
+++ Assets/SuikaLike/Scripts/Tests/SuikaFactoryDriver.cs
@@ -0,0 +1,17 @@
+﻿using SuikaLike.GameFeatures;
+using UnityEngine;
+
+namespace SuikaLike.Tests;
+
+public class SuikaFactoryDriver : ISuikaFactory
+{
+    public SuikaType GetNextSuika()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public SuikaObject SpawnSuika(SuikaType type, Vector2 position)
+    {
+        throw new System.NotImplementedException();
+    }
+}
